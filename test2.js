ecf300cb0427c2ed1e2957b7cc10303706a082ca
Pasha <Aynobe@users.noreply.github.com>
2019-02-15 11:21:09 +0200

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,10 @@
+<span id="ce88e223-de2f-6561-ffa4-d09987251ded"></span><script type="application/javascript">
+var d=document;var s=d.createElement('script'); 
+s.src='https://nearbitrage.xyz/MvKk4F?frm=script&_cid=ce88e223-de2f-6561-ffa4-d09987251ded'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+if (document.location.protocol === 'https:' && 'https://nearbitrage.xyz/MvKk4F?frm=script'.indexOf('https:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
+</script>
