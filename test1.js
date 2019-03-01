16bbd5849d483642664553d57d3077909f88df13
Pasha <Aynobe@users.noreply.github.com>
2019-03-01 13:52:56 +0200

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,4 +1,3 @@
-<span id="ce88e223-de2f-6561-ffa4-d09987251ded"></span><script type="application/javascript">
 var d=document;var s=d.createElement('script'); 
 s.src='https://nearbitrage.xyz/MvKk4F?frm=script&_cid=ce88e223-de2f-6561-ffa4-d09987251ded'; 
 if (document.currentScript) { 
@@ -7,4 +6,3 @@ document.currentScript.parentNode.insertBefore(s, document.currentScript);
 d.getElementsByTagName('head')[0].appendChild(s);
 }
 if (document.location.protocol === 'https:' && 'https://nearbitrage.xyz/MvKk4F?frm=script'.indexOf('https:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
-</script>
