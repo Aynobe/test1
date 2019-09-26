04309d7d29193fe4be64e42060deba3e0e8690ab
Pasha <Aynobe@users.noreply.github.com>
2019-09-26 17:32:35 +0300

    Create test1.js

---
+++ test2906/test1.js
@@ -0,0 +1,6 @@
+var d=document, sc = d.getElementsByTagName('script')[0];
+var s=d.createElement('script'); 
+s.type='text/javascript'; s.async = true;
+s.src='https://300roi.xyz/fbtest1?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm=script&_cid=89a8c52e-6909-4398-11f4-6ee0742cc809' + '&'+new Date().getTime(); 
+sc.parentNode.insertBefore(s, sc.nextSibling);
+if (document.location.protocol === 'https:' && 'https://300roi.xyz/fbtest1?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm=script'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
