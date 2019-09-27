c75010a57e5f96cf13bcff110f89875b48c10450
Pasha <Aynobe@users.noreply.github.com>
2019-09-27 21:11:12 +0300

    Update test1.js

--- test2906/test1.js
+++ test2906/test1.js
@@ -1,6 +1,8 @@
-var d=document, sc = d.getElementsByTagName('script')[0];
-var s=d.createElement('script'); 
-s.type='text/javascript'; s.async = true;
-s.src='https://300roi.xyz/fbtest1?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm=script&_cid=89a8c52e-6909-4398-11f4-6ee0742cc809' + '&'+new Date().getTime(); 
-sc.parentNode.insertBefore(s, sc.nextSibling);
+var d=document;var s=d.createElement('script'); 
+s.src='https://300roi.xyz/fbtest1?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm=script&_cid=93fe8e40-4b82-fa05-f885-2a39b3cb3de5'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
 if (document.location.protocol === 'https:' && 'https://300roi.xyz/fbtest1?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm=script'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
